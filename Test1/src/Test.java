721f629adeb0e830382b0c88ac4295dd2ee16ef9
YSYUP <2272192361@qq.com>
2020-11-18 15:49:46 +0800

    project upload to github

---
+++ Test1/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+	public static void main(String[] args) {
+		long l = 500;
+		float f = 1.1F;
+		double d = 2.2D;
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+	}
+}
